3021ef6d86857163a6e439500b0c7a75333bc8b0
pasted0 <pastedv1ontop@gmail.com>
2026-05-27 06:32:45 +0700

    Wrong endpoint in installer.lua

--- Installer.lua
+++ Installer.lua
@@ -1,6 +1,3 @@
-local http = http or request or (syn and syn.request) or (http and http.request) or (fluxus and fluxus.request) or (getgenv().request)
-local httpService = game:GetService("HttpService")
-
 if isfolder('VapeLite') then
     delfolder('VapeLite')
 end
@@ -8,12 +5,11 @@ end
 
 
 makefolder('VapeLite')
-writefile('VapeLite/MainScript.lua', game:HttpGet("https://raw.githubusercontent.com/pasted0/vapelite/main/MainScript.lua"))
+writefile('VapeLite/MainScript.lua', game:HttpGet("https://raw.githubusercontent.com/pasted0/VapeLiteForRoblox/refs/heads/main/MainScript.lua"))
 makefolder('VapeLite/CustomModules')
-
-
-
-
 makefolder('VapeLite/Profiles')
+
+
+
 
 loadstring(readfile('VapeLite/MainScript.lua'))()
